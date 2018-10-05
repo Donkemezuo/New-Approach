edd1537da52dc3440c1aa063e540af08e8c0857e
Donkemezuo <raymonddonkemezuo.org>
2018-10-05 16:49:58 -0400

    A more simplier approach

---
+++ Tari/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tari
+//
+//  Created by Donkemezuo Raymond Tariladou on 10/5/18.
+//  Copyright © 2018 Donkemezuo Raymond Tariladou. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
